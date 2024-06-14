0502fa679a217366e79bc579a84220c7ac5e9b86
Isaiah-Mwinga <motongori@marketforce360.com>
2024-06-14 16:31:06 +0300

    service

--- customer_orders/api/views.py
+++ customer_orders/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from .tasks import send_order_alert
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        order = serializer.save()
+        send_order_alert(order.customer, order)
+
